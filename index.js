7566eeb8f37499003100b92a7b2f874317cf9701
sankalpsid19 <sankalpyadav96@gmail.com>
2024-10-13 14:00:41 +0530

    vercel.json file updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const uploadRoutes = require("./routes/uploadRoutes"); // Ensure this path is correct
+const uploadRoutes = require("./src/routes/uploadRoutes"); // Ensure this path is correct
 const cors = require("cors");
 
 dotenv.config();
