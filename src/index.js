c9ab2fc5a0326a288a7f2f4018eea0a0f23658d5
sankalpsid19 <sankalpyadav96@gmail.com>
2024-10-13 12:59:48 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -12,14 +12,19 @@ app.use(express.json());
 
 // Use the upload routes
 app.use("/api", uploadRoutes); // This will prefix all routes in uploadRoutes with /api
+
+// Simple route for health check
 app.get("/", (req, res) => {
   res.json({ message: "Pong!" });
 });
+
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
